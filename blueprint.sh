8db271c3baf13d972c01570d83b32c45db1ce4d1
Simon Elliston Ball <simon@simonellistonball.com>
2018-09-27 02:49:06 +0100

    Added initial version of compact template

--- blueprint.sh
+++ blueprint.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 
-cb blueprint delete --name HCP 
-cb blueprint create from-file --file blueprint.json --name HCP --description "Hortonworks Cybersecuriy Platform"
+cb blueprint delete --name "HCP"
+cb blueprint create from-file \
+  --file blueprint.json \
+  --name "HCP" \
+  --description "Hortonworks Cybersecuriy Platform"
+
+
+cb blueprint delete --name "HCP - Compact" 
+cb blueprint create from-file \
+  --file blueprint-compact.json \
+  --name "HCP - Compact" \
+  --description "Development version of Hortonworks Cybersecuriy Platform for trial in smaller environments"
